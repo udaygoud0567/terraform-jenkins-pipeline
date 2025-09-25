375ac3017cde98ca2229e604929a5294ef71b8df
udaygoud0567 <udaykumarcloud08@gmail.com>
2025-09-25 16:18:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "public_instance" {
- ami           = var.ami
- instance_type = var.instance_type
-
- tags = {
-   Name = var.name_tag,
- }
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-0d29db40033c01018"   # ✅ Explicit subnet
+  tags = {
+    Name = "My EC2 Instance"
+  }
 }
